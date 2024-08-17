dfb100ac08bed6dd997c59bde0756170fceba268
zebus3d <zebus3dream@gmail.com>
2024-08-17 15:25:16 +0200

    primer script para cuerdos

---
+++ CuerdOS/zebus/install_kde_from_legacy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Instalar plasma (sin metapaquete y sin konqueror) y desinstalar xfce:
+sudo apt install plasma-desktop plasma-workspace dolphin kate kdialog kfind konsole udisks2 upower kwin-x11 sddm xserver-xorg ark gwenview kcalc kde-spectacle knotes kwalletmanager okular plasma-dataengines-addons plasma-pa plasma-runners-addons plasma-wallpapers-addons plasma-widgets-addons polkit-kde-agent-1 sweeper plasma-nm skanlite apper print-manager fonts-symbola iw x11-apps x11-session-utils xinit xorg
+
+Desinstalar xfce:
+dpkg -l | grep .xfce. | awk '{print $2}' | xargs sudo apt-get purge -V --auto-remove -yy
+dpkg -l | grep gnome | awk '{print $2}' | xargs sudo apt-get purge -V --auto-remove -yy
