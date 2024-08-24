c386df0c59d21bf8551ba83f9962d412a5800c7f
zebus3d <zebus3dream@gmail.com>
2024-08-24 13:56:26 +0200

    algunos cambios

--- Arch/zebus/fix_certificates.sh
+++ Arch/zebus/fix_certificates.sh
@@ -18,39 +18,14 @@ function install_package() {
 }
 
 pacman_packages=(
-    "wayland"
-    "xorg-xwayland" 
-    "qt6-wayland"
-    "plasma-desktop"
-    "plasma-pa"
-    "plasma-nm"
-    "kscreen"
-    "breeze-gtk"
-    "plasma-systemmonitor"
-    "kde-gtk-config"
-    "dolphin"
-    "ark"
-    "gwenview"
-    "kate"
-    "kcalc"
-    "konsole"
-    "qbittorrent"
-    "okular"
-    "spectacle"
-    "p7zip"
-    "pipewire-jack"
-    "pipewire"
-    "gst-plugin-pipewire"
-    "pipewire-audio"
-    "wireplumber"
-    "gst-libav"
-    "ffmpeg4.4"
-    "sddm"
-    "kdegraphics-thumbnailers"
-    "kio-extras"
-    "ffmpegthumbs"
+    "reflector"
+    "rsync"
 )
 
 for package in "${pacman_packages[@]}"; do
     install_package "$package"
 done
+
+echo "Ejecutando reflector:"
+sudo reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
--- Arch/zebus/install_nvidia_driver.sh
+++ Arch/zebus/install_nvidia_driver.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-sudo pacman -S base-devel linux-headers nvidia-dkms
+function install_package() {
+  
+    local package_name=$1
+
+    # Comprobar si el paquete ya está instalado
+    if command -v $package_name &> /dev/null || pacman -Qs $package_name | grep "local/$package_name" &> /dev/null; then
+    
+        echo "✅ $package_name ya está instalado, no se hace nada."
+    
+    else
+        
+        echo "💾 Instalando $package_name..."
+        sudo pacman -S --noconfirm $package_name
+
+    fi
+}
+
+pacman_packages=(
+    "base-devel"
+    "linux-headers"
+    "nvidia-dkms"
+)
+
+for package in "${pacman_packages[@]}"; do
+    install_package "$package"
+done
