d1e240856c821c462fd06076a999ac310bc4a89b
Rodolfo Valentino <165349377+Rodolfo-Valentino@users.noreply.github.com>
2024-11-01 04:24:09 -0300

    Update plasma.sh
    Se agrega AMD-ucode e Intel-ucode

--- Arch/rodo/plasma.sh
+++ Arch/rodo/plasma.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S plasma-desktop plasma-pa powerdevil plasma-nm kscreen sddm sddm-kcm --noconfirm
 
-sudo pacman -S kwalletmanager kdeplasma-addons spectacle dolphin konsole gwenview kate okular vlc qbittorrent ark unrar p7zip partitionmanager plasma-systemmonitor kcalc kde-gtk-config breeze-gtk plasma-framework5 kinfocenter grub-customizer firefox firefox-i18n-es-ar kdenlive obs-studio audacity libreoffice-fresh-es hunspell-es_ar telegram-desktop audacious gimp --noconfirm
+sudo pacman -S amd-ucode intel-ucode kwalletmanager kdeplasma-addons spectacle dolphin konsole gwenview kate okular vlc qbittorrent ark unrar p7zip partitionmanager plasma-systemmonitor kcalc kde-gtk-config breeze-gtk plasma-framework5 kinfocenter grub-customizer firefox firefox-i18n-es-ar kdenlive obs-studio audacity libreoffice-fresh-es hunspell-es_ar telegram-desktop audacious gimp --noconfirm
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
 
