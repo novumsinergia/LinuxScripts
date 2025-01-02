ea5dfe1e77b9606c1b1e2aaf2f802e996b228cd7
Nacho-Telmo <ignacioezcurra37@gmail.com>
2025-01-02 19:56:58 -0300

    Update postinstall.sh
    se agrega la no confirmación en la desistalación de xterm

--- Arch/Nacho/postinstall.sh
+++ Arch/Nacho/postinstall.sh
@@ -6,7 +6,7 @@ sudo pacman -S plasma-framework5 --noconfirm
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
 
 
-sudo pacman -Rs xterm
+sudo pacman -Rs xterm --noconfirm
 
 
 
