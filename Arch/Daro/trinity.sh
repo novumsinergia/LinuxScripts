116021b15df8094c6635ae2d4e066581e17771b7
Daro <elcuchy@gmail.com>
2025-03-15 18:19:21 -0300

    Update trinity.sh
    mejora 4

--- Arch/Daro/trinity.sh
+++ Arch/Daro/trinity.sh
@@ -5,7 +5,7 @@ sudo pacman -S sudo pacman-key --lsign-key  D6D6FAA25E9A3E4ECD9FBDBEC93AF1698685
 
 sudo pacman -S sudo pacman -Sy --noconfirm
 
-sudo pacman -S sudo pacman -S tde-meta sddm --noconfirm
+sudo pacman -S sudo pacman -S tde-meta --noconfirm
 
 sudo pacman -S amd-ucode intel-ucode okular vlc  ark unrar p7zip grub-customizer firefox firefox-i18n-es-ar kdenlive obs-studio audacity libreoffice-fresh-es hunspell-es_uy telegram-desktop audacious gimp zsh zsh-completions --noconfirm
 
@@ -15,7 +15,6 @@ sudo sed -i.bak "63s/.*/GRUB_DISABLE_OS_PROBER="false"/" /etc/default/grub
 rm -rf ~/LinuxScripts
 
 
-
-sudo systemctl enable sddm.service
+sudo pacman -S sddm sddm-kcm  --noconfirm --needed;  systemctl enable sddm.service
 
 grub-mkconfig -o /boot/grub/grub.cfg
