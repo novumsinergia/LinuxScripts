589f7039c4b397231582439fc2057463821b4042
Daro <elcuchy@gmail.com>
2025-03-15 18:13:54 -0300

    Update trinity.sh
    sin yay

--- Arch/Daro/trinity.sh
+++ Arch/Daro/trinity.sh
@@ -5,13 +5,10 @@ sudo pacman -S sudo pacman-key --lsign-key  D6D6FAA25E9A3E4ECD9FBDBEC93AF1698685
 
 sudo pacman -S sudo pacman -Sy --noconfirm
 
-sudo pacman -S sudo pacman -S tde-meta --noconfirm
+sudo pacman -S sudo pacman -S tde-meta sddm --noconfirm
 
-sudo pacman -S sddm amd-ucode intel-ucode okular vlc  ark unrar p7zip grub-customizer firefox firefox-i18n-es-ar kdenlive obs-studio audacity libreoffice-fresh-es hunspell-es_uy telegram-desktop audacious gimp zsh zsh-completions --noconfirm
+sudo pacman -S amd-ucode intel-ucode okular vlc  ark unrar p7zip grub-customizer firefox firefox-i18n-es-ar kdenlive obs-studio audacity libreoffice-fresh-es hunspell-es_uy telegram-desktop audacious gimp zsh zsh-completions --noconfirm
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
-
-yay -S stacer-bin --noconfirm
 sudo pacman -S ntfs-3g os-prober --noconfirm
 sudo sed -i.bak "63s/.*/GRUB_DISABLE_OS_PROBER="false"/" /etc/default/grub
 
